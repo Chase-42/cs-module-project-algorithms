98d2e70616f11a00b50e8e3bda6a3c7a67be6ee9
Chase-42 <chaseofthecollins@gmail.com>
2020-09-08 17:59:31 -0600

    moving_zeroes.py done

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,19 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
-
-    pass
+    y = len(arr)-1
+    for x in range(0, len(arr) -1):
+        while arr[x] == 0:
+            if arr[y] != 0: 
+                # Swap em
+                arr[x], arr[y] = arr[y], arr[x]
+                # If x == y, return 
+            elif x ==y:
+                return arr
+            else: 
+                # move y index to right
+                y -=1
+    return arr
 
 
 if __name__ == '__main__':
