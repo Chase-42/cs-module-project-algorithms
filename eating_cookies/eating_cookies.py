d757d856dd757d5cd70ec27130d00d2c88e02d84
Chase-42 <chaseofthecollins@gmail.com>
2020-09-09 19:59:14 -0600

    eating_cookies.py done

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,11 +2,20 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
+def eating_cookies(n, cache=None):
+    # Base case (no more cookies)
+    if n == 0:
+        return 1 
+    if n < 0: 
+        return 0 
 
-    pass
-
+    if cache is None: 
+        cache = {}
+    # If we've already seen this iteration
+    if n in cache: 
+        return cache[n]
+    
+    return eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
